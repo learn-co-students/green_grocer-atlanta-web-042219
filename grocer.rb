25614bb2f9a5775d61a927d597c5db9a44f6c8fa
han xu <hanxu27@gmail.com>
2019-04-16 19:29:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -7,7 +7,7 @@ def consolidate_cart(cart)
   cart.each do |item|
     if !output.has_key?(item.keys[0])
       output[item.keys[0]] = item.values[0]
-      output[:count] = 1
+      output[item.keys[0][:count]] = 1
     binding.pry
     
     item
